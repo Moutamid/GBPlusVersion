7fedac189046e80cde601bf28ba4a660daccd7d2
suleman <sunnybajwa904@gmail.com>
2022-11-26 11:36:47 +0500

    Commit

--- app/src/main/java/com/moutamid/gbplusversion/BlankMessagesActivity.java
+++ app/src/main/java/com/moutamid/gbplusversion/BlankMessagesActivity.java
@@ -2,12 +2,21 @@ package com.moutamid.gbplusversion;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Toast;
 
 import com.moutamid.gbplusversion.databinding.ActivityBlankMessagesBinding;
 
 public class BlankMessagesActivity extends AppCompatActivity {
     ActivityBlankMessagesBinding binding;
+    String space = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +26,69 @@ public class BlankMessagesActivity extends AppCompatActivity {
         binding.backbtn.setOnClickListener(v -> {
             onBackPressed();
             finish();
+        });
+
+        binding.copyBtn.setOnClickListener(v -> {
+            int sdk = android.os.Build.VERSION.SDK_INT;
+            ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+
+            if (binding.blankText.getText().toString().isEmpty()){
+                Toast.makeText(this, "Nothing To Copy", Toast.LENGTH_SHORT).show();
+            } else {
+                if(sdk < android.os.Build.VERSION_CODES.HONEYCOMB) {
+                    clipboard.setText(binding.blankText.getText().toString());
+                    Toast.makeText(this, "Copied To Clipboard", Toast.LENGTH_SHORT).show();
+                } else {
+                    android.content.ClipData clip = android.content.ClipData.newPlainText("Blank Message", binding.blankText.getText().toString());
+                    clipboard.setPrimaryClip(clip);
+                    Toast.makeText(this, "Copied To Clipboard", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        binding.shareBtn.setOnClickListener(v -> {
+            Intent whatsappIntent = new Intent("android.intent.action.SEND");
+            whatsappIntent.setType("text/plain");
+            whatsappIntent.putExtra("android.intent.extra.TEXT", binding.blankText.getText().toString());
+            try {
+                startActivity(whatsappIntent);
+            } catch (ActivityNotFoundException e) {
+                Toast.makeText(BlankMessagesActivity.this, "Some problems", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        binding.deleteBtn.setOnClickListener(v -> {
+            binding.length.setText("");
+            binding.blankText.setText("");
+            binding.blankText.setVisibility(View.GONE);
+        });
+
+        binding.length.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                int j = Integer.parseInt(s.toString());
+                if (binding.newLineSwitch.isChecked()){
+                    for (int i=0; i<j; i++){
+                        space = space + "\u3164" + "\n";
+                    }
+                } else {
+                    for (int i =0; i<j; i++){
+                        space = space + "\u3164";
+                    }
+                }
+                binding.blankText.setVisibility(View.VISIBLE);
+                binding.blankText.setText(space);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
         });
 
     }
--- app/src/main/java/com/moutamid/gbplusversion/DirectChatActivity.java
+++ app/src/main/java/com/moutamid/gbplusversion/DirectChatActivity.java
@@ -1,9 +1,18 @@
 package com.moutamid.gbplusversion;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.futuremind.recyclerviewfastscroll.Utils;
 import com.moutamid.gbplusversion.databinding.ActivityDirectChatBinding;
 
 public class DirectChatActivity extends AppCompatActivity {
@@ -15,9 +24,32 @@ public class DirectChatActivity extends AppCompatActivity {
         binding = ActivityDirectChatBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        if (!(ContextCompat.checkSelfPermission(this, "android.permission.CAMERA") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.WRITE_EXTERNAL_STORAGE") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.READ_EXTERNAL_STORAGE") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.ACCESS_NETWORK_STATE") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.SET_WALLPAPER") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.INTERNET") == 0 && ContextCompat.checkSelfPermission(this, "android.permission.SYSTEM_ALERT_WINDOW") == 0) && Build.VERSION.SDK_INT >= 23) {
+            requestPermissions(new String[]{"android.permission.CAMERA", "android.permission.WRITE_EXTERNAL_STORAGE", "android.permission.READ_EXTERNAL_STORAGE", "android.permission.ACCESS_NETWORK_STATE", "android.permission.SET_WALLPAPER", "android.permission.INTERNET", "android.permission.SYSTEM_ALERT_WINDOW"}, 0);
+        }
+
+        binding.send.setOnClickListener(v -> {
+            if (binding.phoneNumber.getText().toString().isEmpty()){
+                Toast.makeText(this, "Please add number", Toast.LENGTH_SHORT).show();
+            } else {
+                String Num = binding.countryPicker.getSelectedCountryCode() + binding.phoneNumber.getText().toString();
+                String messg = binding.message.getText().toString();
+                openWhatsApp(Num, messg);
+            }
+        });
+
         binding.backbtn.setOnClickListener(v -> {
             onBackPressed();
             finish();
         });
+    }
+
+    private void openWhatsApp(String smsNumber, String text) {
+        Log.d(TAG, "openWhatsApp: smsNumber: " + smsNumber);
+        Log.d(TAG, "openWhatsApp: text: " + text);
+
+        startActivity(new Intent(Intent.ACTION_VIEW,
+                Uri.parse(String.format("https://api.whatsapp.com/send?phone=%s&text=%s",
+                        smsNumber, text))));
     }
 }
